2b66944aeca709adcb0601489766fa22157e9e0a
nconduah <nconduah@gmail.com>
2015-03-31 11:24:34 -0400

    Added EventMager class holder

---
+++ PulseApp/PulseApp/Common/EventManager.cs
@@ -0,0 +1,36 @@
+﻿using PulseApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseApp.Common
+{
+    public class EventManager
+    {
+        public void CreateEvent(Event pulseEvent, List<User> intendedAttendees)
+        {
+
+        }
+
+        public void UpdateEvent(Event pulseEvent, List<User> intendedAttendees)
+        {
+
+        }
+
+        public void UpdateMyStatus(Guid eventID, int distance)
+        {
+
+        }
+
+        public void GetMyEvents(Guid userID)
+        {
+
+        }
+        public void GetEventStatus(Guid eventID)
+        {
+
+        }
+    }
+}
